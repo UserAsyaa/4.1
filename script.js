22e069928e6f28ece48fd8c3209f20cd5e135f3e
Aysan <asyababayeva18@mail.ru>
2024-08-21 19:30:58 +0300

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const openCallButton = document.querySelector(".left-aside__button--call");   //кнопка трубки
 const callModal = document.querySelector(".call-modal");                     //блок модалки
-const closeCallButton = document.querySelector("call-modal__button-close");  // кнопка с крестиком "закрыть"                
+const closeCallButton = document.querySelector(".call-modal__button-close");  // кнопка с крестиком "закрыть"                
 
 
 openCallButton.addEventListener("click", function(evt){                    
@@ -9,4 +9,23 @@ openCallButton.addEventListener("click", function(evt){
 });
 
 
-closeCallButton.addEventListener('click', e => console.log(e.target));
+ closeCallButton.addEventListener("click", function(){
+    callModal.classList.remove("call-modal--open");
+ })             
+
+
+
+const openFeedbackButton = document.querySelector(".left-aside__button--chat");  
+const feedbackModal = document.querySelector(".feedback-modal");                     
+const closeFeedbackButton = document.querySelector(".feedback-modal__button-close");                  
+
+
+openFeedbackButton.addEventListener("click", function(evt){                    
+    evt.preventDefault();
+    feedbackModal.classList.toggle("feedback-modal--open");                            
+});
+
+
+ closeFeedbackButton.addEventListener("click", function(){
+    feedbackModal.classList.remove("feedback-modal--open");
+ })      
